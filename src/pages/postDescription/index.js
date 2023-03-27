8c0d76f173d68f4f5f7572cba8f946c34a15fcca
Wellington Liviz <113310983+WellingtonLiviz@users.noreply.github.com>
2023-03-26 23:45:25 -0300

    correção de erros

--- src/pages/postDescription/index.js
+++ src/pages/postDescription/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import {  useParams } from "react-router-dom";
 import { loadComments } from "../../services";
 
 import './PostDesciption.css';
